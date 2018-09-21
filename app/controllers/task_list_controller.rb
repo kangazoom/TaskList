cadcfa7bdfda0a7d6ec6c49169c2d34be40eeaa3
kangazoom <michellelcronin@gmail.com>
2018-09-21 02:04:54 -0700

    added edit and update functions

--- app/controllers/task_list_controller.rb
+++ app/controllers/task_list_controller.rb
@@ -16,20 +16,34 @@ class TaskListController < ApplicationController
 
   def create
     @task = Task.new(
-      name: params[:name],
-      description: params[:description],
-      completion_date: params[:completion_date]
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
     )
 
     is_successful_save = @task.save
 
     if is_successful_save
-      redirect_to task_list_path
+      redirect_to tasks_path
     else
       render :new
 
     end
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
 
+# TODO: dry this up w new?
+  def update
+  @task = Task.find(params[:id])
+  @task.update(
+    name: params[:task][:name],
+    description: params[:task][:description],
+    completion_date: params[:task][:completion_date]
+  )
+
+  redirect_to task_path(@task)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    get '/task_list', to: 'task_list#index', as: 'task_list'
-    get '/task_list/new', to: 'task_list#new', as: 'new_task'
-    post '/task_list', to: 'task_list#create'
+    get '/tasks', to: 'task_list#index', as: 'tasks'
+    get '/tasks/new', to: 'task_list#new', as: 'new_task'
+    post '/tasks', to: 'task_list#create'
 
-    get '/task_list/:id', to: 'task_list#show', as: 'task'
+    get '/tasks/:id', to: 'task_list#show', as: 'task'
+    get '/tasks/:id/edit', to: 'task_list#edit', as: 'edit_task'
+    patch '/tasks/:id', to: 'task_list#update'
 
   end
